c08e300e94ce864540627cdc68a05ea3f3943af7
jonslack22 <jonathanslack89@ymail.com>
2023-03-22 21:23:52 +0000

    Movie model and superuser created for admin panel

--- movie_booking/models.py
+++ movie_booking/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200, unique=False)
+    slug = models.SlugField(unique=True)
+    featured_image = CloudinaryField('image', default='placeholder')
+    description = models.TextField()
+    show_date = models.DateField()
+    show_times = models.TimeField()
+
+    class Meta:
+        ordering = ['show_times']
+
+    def __str__(self):
+        return self.title
