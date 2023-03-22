c8af87a834917bb1e8c466ba05aeed798926f8bc
jonslack22 <jonathanslack89@ymail.com>
2023-03-22 21:43:55 +0000

    Added Showtime model and edited admin panel to allow creation of multiple time and date showings per movie

--- movie_booking/admin.py
+++ movie_booking/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
-from .models import Movie
+from .models import Showtime, Movie
 
-admin.site.register(Movie)
+
+class ShowtimeInline(admin.TabularInline):
+    model = Showtime
+    extra = 1
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    inlines = [ShowtimeInline]
+    prepopulated_fields = {'slug': ('title',)}
--- movie_booking/models.py
+++ movie_booking/models.py
@@ -2,16 +2,27 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 
 
+class Showtime(models.Model):
+    movie = models.ForeignKey(
+        'Movie', on_delete=models.CASCADE, related_name='movie_showtimes')
+    show_date = models.DateField(auto_now=False)
+    show_times = models.TimeField(auto_now=False)
+
+    class Meta:
+        unique_together = ('movie', 'show_date', 'show_times')
+
+    def __str__(self):
+        return f'{self.movie.title} - {self.show_date} - {self.show_times}'
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=200, unique=False)
     slug = models.SlugField(unique=True)
-    featured_image = CloudinaryField('image', default='placeholder')
     description = models.TextField()
-    show_date = models.DateField()
-    show_times = models.TimeField()
+    featured_image = CloudinaryField('image', default='placeholder')
 
     class Meta:
-        ordering = ['show_times']
+        ordering = ['title']
 
     def __str__(self):
         return self.title
